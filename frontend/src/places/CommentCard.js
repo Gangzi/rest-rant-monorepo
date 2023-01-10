a71d17a6618d0569c14aecf1d8797bf235750db6
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2023-01-09 19:36:34 -0500

    Only leave a comment when logged in, and automatically be attributed as the author of the comment without having to select an author from a dropdown list

--- frontend/src/places/CommentCard.js
+++ frontend/src/places/CommentCard.js
@@ -1,6 +1,19 @@
+import { useContext } from "react";
+import { CurrentUser } from "../contexts/CurrentUser"
 
 function CommentCard({ comment, onDelete }) {
-    return (
+    const {currentUser} = useContext(CurrentUser)
+
+    let deleteButton = null;
+
+    if (currentUser?.userId === comment.authorId) {
+        deleteButton = (
+            <button className="btn btn-danger" onClick={onDelete} >
+                Delete Comment
+            </button>
+        )
+        }
+return(
         <div className="border col-sm-4">
             <h2 className="rant">{comment.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
             <h4>{comment.content}</h4>
